839826bac863d6bfd457c60738709580c5bd812d
Aarni Koskela <akx@iki.fi>
2017-02-07 11:51:05 +0200

    Configs

--- fish/config.fish
+++ fish/config.fish
@@ -7,4 +7,4 @@ test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shel
 set -x CUDA_HOME /usr/local/cuda
 set -x DYLD_LIBRARY_PATH "$DYLD_LIBRARY_PATH:$CUDA_HOME/lib"
 set -x PATH "$CUDA_HOME/bin" $PATH
-set -g fish_user_paths "/usr/local/bin" $fish_user_paths
+set -g fish_user_paths "$HOME/.cargo/bin" "/usr/local/bin" $fish_user_paths
