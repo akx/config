0ff8f5ad1a7f18873b0797d4030537a9cf35f0b6
Aarni Koskela <akx@iki.fi>
2017-12-03 21:01:37 +0200

    sync

--- fish/config.fish
+++ fish/config.fish
@@ -3,8 +3,9 @@ eval (envfile2fish)
 set __fish_git_prompt_showdirtystate 1
 set __fish_git_prompt_showcolorhints 1
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
-#[ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
 set -x CUDA_HOME /usr/local/cuda
 set -x DYLD_LIBRARY_PATH "$DYLD_LIBRARY_PATH:$CUDA_HOME/lib"
 set -x PATH "$CUDA_HOME/bin" $PATH
 set -g fish_user_paths "$HOME/.cargo/bin" "/usr/local/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/gettext/bin" $fish_user_paths
+set -g fish_user_paths "/Users/akx/.local/bin" $fish_user_paths
